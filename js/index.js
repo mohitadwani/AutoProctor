7856fe0f42f84002a5740ba07d591df0135bb76e
root <raunak.it@gmail.com>
2017-04-06 15:24:13 +0530

    added basic trackingjs

--- js/index.js
+++ js/index.js
@@ -1,5 +1,35 @@
 'use strict';
 (function() {
+    window.onload = function() {
+        var video = document.getElementById('video');
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+        var trackedObjects = new tracking.ObjectTracker('face');
+        // trackedObjects.setInitialScale(3);
+        // trackedObjects.setStepSize(2);
+        // trackedObjects.setEdgesDensity(0.1);
+        trackedObjects.on('track', function(event) {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            if (event.data.length === 0) {
+                console.log('Not Detected');
+            } else {
+                console.log(event.data);
+                event.data.forEach(function(rect) {
+                    context.strokeStyle = '#a64ceb';
+                    context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+                    context.font = '11px Helvetica';
+                    context.fillStyle = "#fff";
+                    context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+                    context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+                });
+            }
+        });
+        tracking.track('#video', trackedObjects, { camera: true });
+        var gui = new dat.GUI();
+        gui.add(trackedObjects, 'edgesDensity', 0.1, 0.5).step(0.01);
+        gui.add(trackedObjects, 'initialScale', 1.0, 10.0).step(0.1);
+        gui.add(trackedObjects, 'stepSize', 1, 5).step(0.1);
+    };
     angular.module('indexApp', []);
     angular.module('indexApp').controller('indexCtrl', function() {
         var vm = this;
@@ -13,7 +43,7 @@
             video: true
         };
 
-		if (!isSecureOrigin) {
+        if (!isSecureOrigin) {
             alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' + '\n\nChanging protocol to HTTPS');
             location.protocol = 'HTTPS';
         }
