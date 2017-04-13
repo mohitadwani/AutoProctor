541de317fd2c87785cbdbe5361b843fdded1ca42
root <raunak.it@gmail.com>
2017-04-13 14:38:19 +0530

    recording done

--- js/index.js
+++ js/index.js
@@ -1,48 +1,66 @@
-'use strict';
 (function() {
     angular.module('indexApp', []);
     angular.module('indexApp').controller('indexCtrl', function() {
         var vm = this;
-        var mediaSource = new MediaSource();
-        var isSecureOrigin = location.protocol === 'https:' || location.hostname === 'localhost';
-        /*var constraints = { // constraints for webrtc
+        /*WebRTC related variables*/
+        var video = document.getElementById('video');
+        var isSecureOrigin;
+        var constraints = { // constraints for webrtc
             audio: true,
             video: true
-        };*/
-        var constraints = {
-            video: {
-                // mozMediaSource: "screen",
-                mediaSource: "screen"
-            },
         };
-        var video = document.getElementById('video');
-        // var canvas = document.getElementById('canvas'); //to draw the face rect
-        // var context = canvas.getContext('2d');
-        // var trackedObjects = new tracking.ObjectTracker('face');
-        // trackedObjects.setInitialScale(3);
-        // trackedObjects.setStepSize(2);
-        // trackedObjects.setEdgesDensity(0.1);
-        // var gui = new dat.GUI(); 
+        // var constraints = {
+        //     video: {
+        //         mozMediaSource: "screen",
+        //         mediaSource: "screen"
+        //     },
+        // };
+        var recordedBlobs;
+        var mediaRecorder;
+        /*Head tracker related variables*/
+        var canvas = document.getElementById('canvas'); //to draw the face rect
+        var context = canvas.getContext('2d');
+        var trackedObjects = new tracking.ObjectTracker('face');
+        // var gui = new dat.GUI();
 
+
+        vm.startRecording = startRecording;
+        vm.stopRecording = stopRecording;
+        vm.download = download;
+
+        var init = init;
+        /*WebRTC related functions*/
         var handleSourceOpen = handleSourceOpen;
         var handleSuccess = handleSuccess;
         var handleError = handleError;
-        var objectOnTrack = objectOnTrack;
+        var handleStop = handleStop;
+        var handleDataAvailable = handleDataAvailable;
+
+        var objectOnTrack = objectOnTrack; // head track listenes
         window.onblur = windowOnblur;
         window.onfocus = windowOnfocus;
 
-        // trackedObjects.on('track',objectOnTrack);
-        /*Shows a control to change the three settings*/
-        // gui.add(trackedObjects, 'edgesDensity', 0.1, 0.5).step(0.01);
-        // gui.add(trackedObjects, 'initialScale', 1.0, 10.0).step(0.1);
-        // gui.add(trackedObjects, 'stepSize', 1, 5).step(0.1);
+        init();
 
-        if (!isSecureOrigin) {
-            alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' + '\n\nChanging protocol to HTTPS');
-            location.protocol = 'HTTPS';
+        function init() {
+            isSecureOrigin = location.protocol === 'https:' || location.hostname === 'localhost';
+            if (!isSecureOrigin) {
+                alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' + '\n\nChanging protocol to HTTPS');
+                location.protocol = 'HTTPS';
+            }
+            // mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
+            navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+
+            trackedObjects.on('track',objectOnTrack);
+            trackedObjects.setInitialScale(1);
+            trackedObjects.setStepSize(2);
+            trackedObjects.setEdgesDensity(0.1);
+
+            /*Shows a control to change the three settings for head tracker*/
+            // gui.add(trackedObjects, 'edgesDensity', 0.1, 0.5).step(0.01);
+            // gui.add(trackedObjects, 'initialScale', 1.0, 10.0).step(0.1);
+            // gui.add(trackedObjects, 'stepSize', 1, 5).step(0.1);
         }
-        mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
-        navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
 
         function handleSourceOpen(event) {
             console.log('MediaSource opened');
@@ -51,9 +69,8 @@
         }
 
         function handleSuccess(stream) {
-            // recordButton.disabled = false;
             console.log('getUserMedia() got stream: ', stream);
-            // tracking.track('#video', trackedObjects, { camera: true });
+            tracking.track('#video', trackedObjects, { camera: true });
             window.stream = stream;
             if (window.URL) {
                 video.src = window.URL.createObjectURL(stream);
@@ -89,6 +106,53 @@
                     context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
                 });
             }
+        }
+
+        function startRecording() {
+            recordedBlobs = [];
+            var options = { mimeType: 'video/webm;codecs=vp9' };
+            try {
+                mediaRecorder = new MediaRecorder(window.stream, options);
+            } catch (e) {
+                console.error('Exception while creating MediaRecorder: ' + e);
+                alert('Exception while creating MediaRecorder: ' + e + '. mimeType: ' + options.mimeType);
+                return;
+            }
+            mediaRecorder.onstop = handleStop;
+            mediaRecorder.ondataavailable = handleDataAvailable;
+            mediaRecorder.start(10); // collect 10ms of data
+            console.log('recording started');
+        }
+
+        function handleStop(event) {
+
+        }
+
+        function handleDataAvailable(event) {
+            if (event.data && event.data.size > 0) {
+                recordedBlobs.push(event.data);
+            }
+        }
+
+        function stopRecording() {
+            mediaRecorder.stop();
+            console.log('Recorded Blobs: ', recordedBlobs);
+            // recordedVideo.controls = true;
+        }
+
+        function download() {
+            var blob = new Blob(recordedBlobs, { type: 'video/webm' });
+            var url = window.URL.createObjectURL(blob);
+            var a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = 'test.webm';
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function() {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 100);
         }
     });
 })();
